37312706a0eeb2fb4fb4a2b2d51633e408c21f70
rafai123 <rafaimhd123@gmail.com>
2023-05-26 16:02:00 +0700

    add margin-left in add button in layout todo

--- src/components/todos/TodoLayout.jsx
+++ src/components/todos/TodoLayout.jsx
@@ -23,7 +23,7 @@ const TodoLayout = () => {
       <form onSubmit={handleCreateTodo}>
         <div className="container flex mx-auto items-center align-middle justify-between sm:max-w-md: md:max-w-xl lg:max-w-2xl">
           <input type="text" value={userInput} onChange={(e) => handleUserInput(e.target.value)} placeholder="Write your todo here" className="border-2 border-gray-400 rounded-lg px-4 py-3 w-full shadow-lg" />
-          <button className="text-5xl shadow-lg border-solid border-2 border-slate-700  rounded-lg hover:text-slate-50 hover:bg-slate-800">
+          <button className="ml-1 text-5xl shadow-lg border-solid border-2 border-slate-700  rounded-lg hover:text-slate-50 hover:bg-slate-800">
             <MdOutlinePlaylistAdd />
           </button>
         </div>
